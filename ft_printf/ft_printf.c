fa9af0534109c6aedbaeab528613deb099d4b874
Morantista <150128730+Morantista@users.noreply.github.com>
2024-07-08 20:01:57 +0200

    Add files via upload

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,68 +1,165 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cballet <cballet@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/01 09:39:18 by cballet           #+#    #+#             */
-/*   Updated: 2023/12/07 18:49:44 by cballet          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdarg.h>
+#include <unistd.h>
 
-#include "ft_printf.h"
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+static int	ft_putnbr_base(unsigned int n, char *base, int size)
+{
+	int	len;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		ft_putnbr_base(n * (-1), base, size);
+	}
+	else if ((n >= 0) && (n < (unsigned int)size))
+		write(1, base + n, 1);
+	else if (n >= (unsigned int)size)
+	{
+		ft_putnbr_base(n / size, base, size);
+		ft_putnbr_base(n % size, base, size);
+	}
+	len = 0;
+	if (n == 0)
+		return (1);
+	while (n != 0)
+	{
+		len++;
+		n = n / size;
+	}
+	return (len);
+}
+
+int	ft_print_hexlow(unsigned int n)
+{
+	char	*base;
+	int		i;
+	int		len;
+
+	base = "0123456789abcdef";
+	i = 0;
+	len = 0;
+	while (base[i])
+		i++;
+	len = ft_putnbr_base(n, base, i);
+	return (len);
+}
+
+static int	ft_print_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	while (n != 0)
+	{
+		len++;
+		n = n / 10;
+	}
+	return (len);
+}
+
+int	ft_print_int(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (11);
+	}
+	else if (n < 0)
+	{
+		ft_putchar('-');
+		ft_print_int(n * (-1));
+		len++;
+	}
+	else if (n >= 0 && n <= 9)
+		ft_putchar(n + 48);
+	else if (n > 9)
+	{
+		ft_print_int(n / 10);
+		ft_print_int(n % 10);
+	}
+	len += ft_print_len(n);
+	return (len);
+}
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	if (!str)
+		return ;
+	while(str[i])
+	{
+		write(1,&str[i], 1);
+		i++;
+	}
+}
+int	ft_print_str(char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (!s)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	while (s[i])
+		i++;
+	ft_putstr(s);
+	return (i);
+}
 
 static int	ft_format(va_list args, const char format)
 {
 	int	printlengh;
 
 	printlengh = 0;
-	if (format == 'c')
-		printlengh += ft_print_char(va_arg(args, int));
-	else if (format == 's')
+	if (format == 's')
 		printlengh += ft_print_str(va_arg(args, char *));
-	else if (format == 'p')
-		printlengh += ft_print_ptr(va_arg(args, unsigned long));
 	else if (format == 'd' || format == 'i')
 		printlengh += ft_print_int(va_arg(args, int));
-	else if (format == 'u')
-		printlengh += ft_print_uns(va_arg(args, unsigned int));
 	else if (format == 'x')
 		printlengh += ft_print_hexlow(va_arg(args, unsigned int));
-	else if (format == 'X')
-		printlengh += ft_print_hexup(va_arg(args, unsigned int));
-	else if (format == '%')
-		printlengh += ft_print_percent();
 	return (printlengh);
 }
 
-int	ft_printf(const char *str, ...)
+int ft_printf(const char *str, ... )
 {
-	int		i;
-	int		printlengh;
+	int	i;
+	int print_len;
 	va_list	args;
-
 	if (!str)
 		return (-1);
 	i = 0;
-	printlengh = 0;
+	print_len = 0;
 	va_start(args, str);
-	while (str[i])
+	while(str[i])
 	{
 		if (str[i] == '%')
 		{
-			printlengh += ft_format(args, str[i + 1]);
+			print_len += ft_format(args, str[i + 1]);
 			i++;
 		}
 		else
-			printlengh += ft_print_char(str[i]);
+			print_len += write(1, &str[i], 1);
 		i++;
 	}
 	va_end(args);
-	return (printlengh);
+	return(print_len);
 }
-int	main(void)
+
+int	main()
 {
-	ft_printf(NULL);
-	return(0);
+	ft_printf("%s\n", "toto");
+	ft_printf("Hexadecimal for %d is %x\n", 42, 42);
+	ft_printf("Magic %s is %d", "number", 42);
+	ft_printf("Magic %f is z", "number");
 }
--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,46 +1,106 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cballet <cballet@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 09:41:04 by cballet           #+#    #+#             */
-/*   Updated: 2024/01/11 19:39:47 by cballet          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-char	*readandcopy(int fd, char *stash)
+int	ft_strlen(char *str)
 {
-	int		bytes_readed;
-	char	*buf;
+	int	i;
 
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buf)
-		return (NULL);
-	bytes_readed = 1;
-	while (!ft_strchr(stash, '\n') && bytes_readed != 0)
-	{
-		bytes_readed = read(fd, buf, BUFFER_SIZE);
-		if (bytes_readed == -1)
-		{
-			free(buf);
-			return (NULL);
-		}
-		buf[bytes_readed] = '\0';
-		stash = ft_strjoin(stash, buf);
-	}
-	free(buf);
-	return (stash);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
+char	*ft_strchr(char *str, char c)
+{
+	int	i;
+
+	if (!str)
+		return (NULL);
+	if (c == '\0')
+		return (str);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (str);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strcpy(char *dest, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char *str, char *join)
+{
+	int		i;
+	int		strlen;
+	int		joinlen;
+	char	*dest;
+
+	if (!str)
+	{
+		str = (char *)malloc(sizeof(char) * 1);
+		str[0] = '\0';
+	}
+	if (!str && !join)
+		return (NULL);
+	strlen = ft_strlen(str);
+	joinlen = ft_strlen(join);
+	dest = (char *)malloc(sizeof(char) * ((strlen + joinlen) + 2));
+	if (!dest)
+		return (NULL);
+	dest = ft_strcpy(dest, str);
+	i = 0;
+	while (join[i])
+	{
+		dest[strlen] = join[i];
+		i++;
+		strlen++;
+	}
+	dest[strlen] = '\0';
+	return (dest);
+}
+
+char	*read_and_copy(int fd, char *stash)
+{
+	int		size_readed;
+	char	*buff;
+
+	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	size_readed = 1;
+	while (!ft_strchr(stash, '\n') && size_readed != 0)
+	{
+		size_readed = read(fd, buff, BUFFER_SIZE);
+		if (size_readed == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[size_readed] = '\0';
+		stash = ft_strjoin(stash, buff);
+	}
+	free (buff);
+	return (stash);
+}
 char	*get_line(char *stash)
 {
-	size_t	i;
-	size_t	size;
 	char	*dest;
+	int		size;
+	int		i;
 
 	size = 0;
 	if (!stash[size])
@@ -62,25 +122,29 @@ char	*get_line(char *stash)
 
 char	*clean_stash(char *stash)
 {
+	char	*dest;
 	int		size;
 	int		i;
-	char	*dest;
 
 	size = 0;
-	i = 0;
-	while (stash[size] && stash[size] != '\n')
-		size++;
 	if (!stash[size])
 	{
-		free(stash);
+		free (stash);
 		return (NULL);
 	}
+	while (stash[size] && stash[size] != '\n')
+		size++;
 	dest = (char *)malloc(sizeof(char) * ((ft_strlen(stash) - size) + 1));
 	if (!dest)
 		return (NULL);
 	size++;
-	while (stash[size] && stash[size] != '\0')
-		dest[i++] = stash[size++];
+	i = 0;
+	while (stash[size])
+	{
+		dest[i] = stash[size];
+		i++;
+		size++;
+	}
 	dest[i] = '\0';
 	free(stash);
 	return (dest);
@@ -88,39 +152,17 @@ char	*clean_stash(char *stash)
 
 char	*get_next_line(int fd)
 {
-	static char	*stash;
-	char		*line;
+	static char *stash;
+	char *line;
 
-	if (BUFFER_SIZE <= 0 || fd < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	stash = readandcopy(fd, stash);
+	stash = read_and_copy(fd, stash);
 	if (!stash)
 		return (NULL);
 	line = get_line(stash);
 	stash = clean_stash(stash);
 	return (line);
 }
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-int	main(void)
-{
-	int		fd;
-	char	*line;
 
-	fd = open("coucou.txt", O_RDWR);
-	if (fd < 0)
-		return (1);
-	line = get_next_line(fd);
-	while (1)
-	{
-		if (line == NULL)
-			break ;
-		printf("|%s|", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	return (0);
-	close(fd);
-}
